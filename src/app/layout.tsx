68ae7942629ab627af8cd9d4a5915429438448de
Snupai (Nya~) <nya@snupai.me>
2025-04-08 22:46:54 +0200

    Add metadata and image for Open Graph and Twitter in layout.tsx
    - Introduced metadata for the page, including title and description.
    - Configured Open Graph and Twitter card settings with relevant details and an image.
    - Added a new image asset for use in the metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,36 @@
 
 import { Public_Sans } from "next/font/google";
 import "../styles/globals.css";
+import type { Metadata } from 'next'
 
 const publicSans = Public_Sans({ 
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-public-sans"
 });
+
+export const metadata: Metadata = {
+  title: 'Hey Miizu!',
+  description: "Hey Miizu, i was wondering if you could show me some of your projects you've been making recently.",
+  openGraph: {
+    title: 'Hey Miizu!',
+    description: "Hey Miizu, i was wondering if you could show me some of your projects you've been making recently.",
+    images: [
+      {
+        url: '/dd8ushtKAafNiPreGQQfuOm10U.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Hey Miizu!'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Hey Miizu!',
+    description: "Hey Miizu, i was wondering if you could show me some of your projects you've been making recently.",
+    images: ['/dd8ushtKAafNiPreGQQfuOm10U.jpg'],
+  }
+}
 
 export default function RootLayout({
   children,
